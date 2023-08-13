4a62c3af429e43dbe7c6f873fe1cc7fb6815e054
João Pedro Araújo <santos0santos0@outlook.com>
2023-08-13 18:08:06 -0300

    feat(mysql): added service to MySQL in docker-compose.yaml

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) VALUES('John')`
+connection.query(sql)
+connection.end()
 
 app.get('/', (req, res) => {
     res.send('Hello World...')
